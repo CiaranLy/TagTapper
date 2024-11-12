83cf0c18edc81b40ede905b2ffa078ba23466d44
Ciaran Lynch <lynchbros3@gmail.com>
2024-11-12 11:45:13 +0000

    removed unused imports

--- app/src/main/java/com/example/tagtapper/MainActivity.java
+++ app/src/main/java/com/example/tagtapper/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.tagtapper;
 
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SearchView;
 
